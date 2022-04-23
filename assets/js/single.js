baa733f26a97bb475760ace104863456205f14bf
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-22 17:42:53 -0700

    able to  switch pages dynamically

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,18 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  console.log(repoName);
+  getRepoIssues(repoName);
+  repoNameEl.textContent = repoName;
+}
+
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -81,4 +93,6 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
+// getRepoIssues();
