1b36b1e3a4e54a6c50c5168e8859cf534f81a143
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-22 17:56:17 -0700

    added redirect incase no repos found

--- assets/js/single.js
+++ assets/js/single.js
@@ -6,12 +6,21 @@ var repoNameEl = document.querySelector("#repo-name");
 
 
 var getRepoName = function() {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  console.log(repoName);
+
+  
+  if (repoName) {
+    // display repo name on the page 
+    repoNameEl.textContent = repoName;
   getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
-}
+  }
+  else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 
 var getRepoIssues = function(repo) {
@@ -32,8 +41,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
